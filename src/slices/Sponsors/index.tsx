71c9446c5436bdd302a0662c5021c4b241f6461a
Ihsen Bouallegue <bouallegueihsen@gmail.com>
2024-09-19 02:04:50 +0200

    Refactor SponsorCarousel component to adjust the width of the left and right gradient backgrounds

--- src/slices/Sponsors/index.tsx
+++ src/slices/Sponsors/index.tsx
@@ -132,7 +132,7 @@ const SponsorCarousel = ({
           </div>
         ))}
       </Marquee>
-      <div className="pointer-events-none absolute inset-y-0 left-0 w-60 bg-gradient-to-r from-white dark:from-background" />
+      <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background" />
       <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-background" />
     </div>
   );
