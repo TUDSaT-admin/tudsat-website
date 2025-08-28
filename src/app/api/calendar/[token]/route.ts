8cf9ad76738ef99b7de88ceb3377064d9ffe7329
Yasamato <agent77326@gmail.com>
2025-08-28 12:25:17 +0200

    feat(Calendar): include @fullcalendar/react

---
+++ src/app/api/calendar/[token]/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest } from "next/server"
+
+export async function GET(
+  _: NextRequest,
+  { params }: { params: Promise<{ token: string }>
+}) {
+    const { token } = await params
+    const response = await fetch(`https://cloud.bvsr.space/remote.php/dav/public-calendars/${token}/?export`)
+    if (response.status !== 200) {
+      console.error("Error fetching calendar data", response.status)
+      return new Response("Not Found", { status: 404 })
+    }
+    const data = await response.text()
+    return new Response(data, {
+        headers: {
+            'Content-Type': 'text/calendar',
+        }
+    })
+}
--- src/slices/Calendar/index.tsx
+++ src/slices/Calendar/index.tsx
@@ -4,7 +4,11 @@ import { useState, useMemo } from "react";
 import Bounded from "@/components/bounded";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
-import { Button } from "@/components/ui/button";
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import iCalendarPlugin from '@fullcalendar/icalendar'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import { EventSourceInput } from "@fullcalendar/core/index.js";
 
 /**
  * Props for `Calendar`.
@@ -18,14 +22,26 @@ type CalendarVariation = CalendarProps["slice"]["variation"];
 const Calendar = ({ slice }: CalendarProps) => {
   const [calendar, setCalendar] = useState<CalendarVariation>(slice.variation);
 
-  const url = useMemo(() => {
+  const eventSource = useMemo<EventSourceInput>(() => {
     if (calendar === "trace") {
-      return "https://cloud.bvsr.space/apps/calendar/p/wqoMRmsWDGxo2t2T";
+      return {
+        url: `/api/calendar/wqoMRmsWDGxo2t2T`,
+        format: "ics",
+        color: "#007243",
+      };
     }
     if (calendar === "rapid") {
-      return "https://cloud.bvsr.space/apps/calendar/p/SFd5igN2ycteTbg3";
+      return {
+        url: `/api/calendar/SFd5igN2ycteTbg3`,
+        format: "ics",
+        color: "#930D16",
+      };
     }
-    return "https://cloud.bvsr.space/apps/calendar/p/TdefRGrLJDJAypKj";
+    return {
+      url: `/api/calendar/TdefRGrLJDJAypKj`,
+      format: "ics",
+      color: "#2F4D86",
+    };
   }, [calendar]);
 
   return (
@@ -33,33 +49,49 @@ const Calendar = ({ slice }: CalendarProps) => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <iframe
-        className="w-full aspect-video"
-        title="Calendar"
-        src={url}
-        style={{
-          border: "none",
-        }}
-      />
-      <div className="flex gap-2 items-center mt-4">
-        <Button
-          onClick={() => setCalendar("default")}
-          disabled={calendar === "default"}
-        >
-          TUDSaT
-        </Button>
-        <Button
-          onClick={() => setCalendar("trace")}
-          disabled={calendar === "trace"}
-        >
-          TRACE
-        </Button>
-        <Button
-          onClick={() => setCalendar("rapid")}
-          disabled={calendar === "rapid"}
-        >
-          RAPID
-        </Button>
+      <div className="w-full aspect-video bg-primary text-primary-foreground rounded-lg p-4">
+        <FullCalendar
+          plugins={[dayGridPlugin, iCalendarPlugin, timeGridPlugin]}
+          initialView="timeGridWeek"
+          events={eventSource}
+          eventTimeFormat={{
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: false,
+          }}
+          weekNumbers={true}
+          weekNumberCalculation="ISO"
+          customButtons={{
+            tudsat: {
+              text: "TUDSaT",
+              click: () => {
+                setCalendar("default");
+              },
+            },
+            trace: {
+              text: "TRACE",
+              click: () => {
+                setCalendar("trace");
+              },
+            },
+            rapid: {
+              text: "RAPID",
+              click: () => {
+                setCalendar("rapid");
+              },
+            },
+          }}
+          headerToolbar={{
+            start: "dayGridMonth,timeGridWeek,timeGridDay",
+            center: "title",
+            end: "today prev,next",
+          }}
+          footerToolbar={{
+            start: "",
+            center: "",
+            end: "tudsat,trace,rapid",
+          }}
+        />
       </div>
     </Bounded>
   );
