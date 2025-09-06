24304a77ad634d87a2652219eec60e506e4720a1
Yasamato <agent77326@gmail.com>
2025-09-06 12:45:22 +0200

    refactor(Calendar): simplify calendar view to timeGridWeek

--- src/slices/Calendar/index.tsx
+++ src/slices/Calendar/index.tsx
@@ -1,36 +1,35 @@
 "use client";
 
-import { useState, useMemo } from "react";
+import { useMemo } from "react";
 import Bounded from "@/components/bounded";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid";
 import iCalendarPlugin from "@fullcalendar/icalendar";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import { EventSourceInput } from "@fullcalendar/core/index.js";
+import { useRouter } from "next/navigation";
 
 /**
  * Props for `Calendar`.
  */
 export type CalendarProps = SliceComponentProps<Content.CalendarSlice>;
 
-type CalendarVariation = CalendarProps["slice"]["variation"];
 /**
  * Component for "Calendar" Slices.
  */
 const Calendar = ({ slice }: CalendarProps) => {
-  const [calendar, setCalendar] = useState<CalendarVariation>(slice.variation);
+  const router = useRouter();
 
   const eventSource = useMemo<EventSourceInput>(() => {
-    if (calendar === "trace") {
+    if (slice.variation === "trace") {
       return {
         url: `/api/calendar/wqoMRmsWDGxo2t2T`,
         format: "ics",
         color: "#007243",
       };
     }
-    if (calendar === "rapid") {
+    if (slice.variation === "rapid") {
       return {
         url: `/api/calendar/SFd5igN2ycteTbg3`,
         format: "ics",
@@ -42,51 +41,52 @@ const Calendar = ({ slice }: CalendarProps) => {
       format: "ics",
       color: "#2F4D86",
     };
-  }, [calendar]);
+  }, [slice.variation]);
+
+  const link = useMemo(() => {
+    if (slice.variation === "trace") {
+      return "https://cloud.bvsr.space/apps/calendar/p/wqoMRmsWDGxo2t2T";
+    }
+    if (slice.variation === "rapid") {
+      return "https://cloud.bvsr.space/apps/calendar/p/SFd5igN2ycteTbg3";
+    }
+    return "https://cloud.bvsr.space/apps/calendar/p/TdefRGrLJDJAypKj";
+  }, [slice.variation]);
 
   return (
-    <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-      <div className="w-full bg-primary text-primary-foreground rounded-lg p-4">
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <div className="w-full bg-primary text-primary-foreground rounded-lg p-4 max-h-screen">
         <FullCalendar
-          plugins={[dayGridPlugin, iCalendarPlugin, timeGridPlugin]}
-          initialView="dayGridMonth"
+          plugins={[iCalendarPlugin, timeGridPlugin]}
+          initialView="timeGridWeek"
           events={eventSource}
           eventTimeFormat={{
             hour: "2-digit",
             minute: "2-digit",
             hour12: false,
           }}
+          editable={false}
+          selectable={false}
+          selectMirror={false}
+          nowIndicator={true}
+          navLinks={false}
           weekNumbers={true}
           weekNumberCalculation="ISO"
           customButtons={{
-            tudsat: {
-              text: "TUDSaT",
+            link: {
+              text: "View full calendar",
               click: () => {
-                setCalendar("default");
-              },
-            },
-            trace: {
-              text: "TRACE",
-              click: () => {
-                setCalendar("trace");
-              },
-            },
-            rapid: {
-              text: "RAPID",
-              click: () => {
-                setCalendar("rapid");
+                router.push(link);
               },
             },
           }}
           headerToolbar={{
-            start: "dayGridMonth,timeGridWeek,timeGridDay",
+            start: "link",
             center: "title",
             end: "today prev,next",
-          }}
-          footerToolbar={{
-            start: "",
-            center: "",
-            end: "tudsat,trace,rapid",
           }}
         />
       </div>
