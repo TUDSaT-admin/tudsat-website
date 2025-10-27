98aef5341cb584299cadcb1a2b0fe7ebd69a7931
TUDSaT-admin <vorstand@tudsat.space>
2025-10-27 17:50:06 +0100

    Update index.tsx
    no title

--- src/slices/Projects/index.tsx
+++ src/slices/Projects/index.tsx
@@ -18,7 +18,6 @@ const Projects = ({ slice }: ProjectsProps) => {
       data-slice-variation={slice.variation}
       withExtraPadding
     >
-      <h2 className="text-4xl font-bold mb-8">Projects</h2>
       <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
         {slice.items.map((item) => (
           <div
